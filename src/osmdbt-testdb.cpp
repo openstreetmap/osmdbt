543e98b69dbb380783ce3a47a1d30b6c63af461e
Jochen Topf <jochen@topf.org>
2020-02-13 13:48:20 +0100

    Show schema version in osmdbt-testdb

--- src/osmdbt-testdb.cpp
+++ src/osmdbt-testdb.cpp
@@ -27,20 +27,32 @@ int main(int argc, char *argv[])
         pqxx::work txn{db};
         vout << "Database version: " << get_db_version(txn) << '\n';
 
-        pqxx::result result =
-            txn.exec("SELECT slot_name, database, confirmed_flush_lsn FROM "
-                     "pg_replication_slots WHERE slot_type = 'logical' AND "
-                     "plugin = 'osm-logical';");
+        {
+            pqxx::result const result =
+                txn.exec("SELECT slot_name, database, confirmed_flush_lsn FROM "
+                        "pg_replication_slots WHERE slot_type = 'logical' AND "
+                        "plugin = 'osm-logical';");
 
-        if (result.empty()) {
-            vout << "Replication not enabled\n";
-        } else {
-            vout << "Active replication slots:\n";
-            for (auto const &row : result) {
-                vout << "  name=" << row[0] << " db=" << row[1]
-                     << " lsn=" << row[2] << '\n';
+            if (result.empty()) {
+                vout << "Replication not enabled\n";
+            } else {
+                vout << "Active replication slots:\n";
+                for (auto const &row : result) {
+                    vout << "  name=" << row[0] << " db=" << row[1]
+                        << " lsn=" << row[2] << '\n';
+                }
             }
         }
+
+        pqxx::result const result =
+            txn.exec("SELECT max(version) FROM schema_migrations WHERE "
+                     "char_length(version) = 14;");
+
+        if (result.size() != 1) {
+            throw std::runtime_error{"Database error: Expected single result"};
+        }
+
+        vout << "Schema version: " << result[0][0].c_str() << '\n';
 
         txn.commit();
 
