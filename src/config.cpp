4693e78fcb00636dc6df2e048ae2982f651f87f4
Jochen Topf <jochen@topf.org>
2020-02-20 17:46:25 +0100

    Refactor out some common code

--- src/config.cpp
+++ src/config.cpp
@@ -2,6 +2,19 @@
 #include "config.hpp"
 #include "exception.hpp"
 
+static void set_config(YAML::Node const &node, std::string& config)
+{
+    if (!node) {
+        return;
+    }
+
+    if (node.IsNull()) {
+        config = "";
+    } else {
+        config = node.as<std::string>();
+    }
+}
+
 Config::Config(std::string const &config_file, osmium::VerboseOutput &vout)
 : m_config{YAML::LoadFile(config_file)}
 {
@@ -10,25 +23,13 @@ Config::Config(std::string const &config_file, osmium::VerboseOutput &vout)
             throw config_error{"'database' entry must be a Map."};
         }
 
-        if (m_config["database"]["host"]) {
-            m_db_host = m_config["database"]["host"].as<std::string>();
-        }
-        if (m_config["database"]["port"]) {
-            m_db_port = m_config["database"]["port"].as<std::string>();
-        }
-        if (m_config["database"]["dbname"]) {
-            m_db_dbname = m_config["database"]["dbname"].as<std::string>();
-        }
-        if (m_config["database"]["user"]) {
-            m_db_user = m_config["database"]["user"].as<std::string>();
-        }
-        if (m_config["database"]["password"]) {
-            m_db_password = m_config["database"]["password"].as<std::string>();
-        }
-        if (m_config["database"]["replication_slot"]) {
-            m_replication_slot =
-                m_config["database"]["replication_slot"].as<std::string>();
-        }
+        set_config(m_config["database"]["host"], m_db_host);
+        set_config(m_config["database"]["port"], m_db_port);
+        set_config(m_config["database"]["dbname"], m_db_dbname);
+        set_config(m_config["database"]["user"], m_db_user);
+        set_config(m_config["database"]["password"], m_db_password);
+        set_config(m_config["database"]["replication_slot"],
+                   m_replication_slot);
     }
 
     if (m_config["log_dir"]) {
