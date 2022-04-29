ea8709b79966bae08a25ec05a8be7bc36b2a2f63
Jochen Topf <jochen@topf.org>
2022-04-29 14:03:26 +0200

    Rename variable to silence clang-tidy warning
    readability-suspicious-call-argument

--- src/util.cpp
+++ src/util.cpp
@@ -24,14 +24,14 @@ void write_data_to_file(std::string const &data, std::string const &dir_name,
                         std::string const &file_name)
 {
     std::string const file_name_final{dir_name + file_name};
-    std::string const file_name_new{file_name_final + ".new"};
+    std::string const file_name_tmp{file_name_final + ".new"};
 
-    int const fd = excl_write_open(file_name_new);
+    int const fd = excl_write_open(file_name_tmp);
 
     osmium::io::detail::reliable_write(fd, data.data(), data.size());
     osmium::io::detail::reliable_fsync(fd);
     osmium::io::detail::reliable_close(fd);
 
-    rename_file(file_name_new, file_name_final);
+    rename_file(file_name_tmp, file_name_final);
     sync_dir(dir_name);
 }
